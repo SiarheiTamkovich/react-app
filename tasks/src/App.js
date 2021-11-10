ba0c09cd5f90fe577824fd7ee5768572241904f1
SiarheiTamkovich <mpg2000@mail.ru>
2021-11-10 21:39:19 +0300

    feat: add my app

--- tasks/src/App.js
+++ tasks/src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import TodoList from "./Todo/TodoList";
+import FavorMovie from "./Task/FavorMovie";
+
+const detailsArray = [
+  { name: "1", detail: "1" },
+  { name: "2", detail: "2" },
+  { name: "3", detail: "3" },
+  { name: "4", detail: "4" },
+  { name: "5", detail: "5" },
+];
 
 function App() {
-  const [arrStart, setTodo] = React.useState([
-    {id: 1, completed: false, title: 'Купить хлеб'},
-    {id: 2, completed: true, title: 'Купить масло'},
-    {id: 3, completed: false, title: 'Купить молоко'},
-  ])
-
-  function toggleTodo(id){
-    setTodo(
-      arrStart.map(item => {
-        if (item.id === id) {
-          item.completed = !item.completed;
-        }
-        return item;
-      })
-    )
-  }
-
+  
   return (<div className='wrapper'>
-    <h1>Task1</h1>
-    <TodoList arrTodo={arrStart} onToggle={toggleTodo}/>
+    <h1>My favorite movie</h1>
+    <FavorMovie data={detailsArray} />
   </div>
   )
 }
